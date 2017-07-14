0ff5c4ab2d6a5c6f85375758a66ef8af3f5d3ef4
Jhun24 <jjanghuni102@naver.com>
2017-07-14 21:27:20 +0900

    add pharmarcy.js /check query

--- routes/pharmarcy.js
+++ routes/pharmarcy.js
@@ -4,14 +4,18 @@ function pharmarcy(app,request){
     //  http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyLcinfoInqire
     //
     
-    var url = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyLcinfoInqire";
-    url += "?ServiceKey"+"BIgnMcOt6Lmw%2FA9LPOyT40EBlmA1hkOm5ntiXi4od57UdwRvmrkK6p2B%2FG7YjxbGo%2FoPK67it2tisgdfLx0Y5g%3D%3D";
-    url +=  '&' + encodeURIComponent('') + '=' + encodeURIComponent('');
-    
-    request({
-        url:url,
-        method:"GET"
-    },(err,response,body)=>{
-        console.log(body);
-    })
+    app.get('/pharmarcy/check',(req,res)=>{
+        "use strict";
+        var url = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyLcinfoInqire";
+        url += "?ServiceKey"+"BIgnMcOt6Lmw%2FA9LPOyT40EBlmA1hkOm5ntiXi4od57UdwRvmrkK6p2B%2FG7YjxbGo%2FoPK67it2tisgdfLx0Y5g%3D%3D";
+        url +=  '&' + encodeURIComponent('') + '=' + encodeURIComponent('');
+
+        request({
+            url:url,
+            method:"GET"
+        },(err,response,body)=>{
+            console.log(body);
+            res.send(body);
+        });
+    });
 }
